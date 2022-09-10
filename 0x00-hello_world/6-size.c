5bf1293fc6f95ee85a332fa80144e4f778c2eaa4
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-09-10 15:00:59 -0700

    print the size of various types in 32 bit env

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+
+/**
+ * Description : Main - prints the sizeof a char is...
+ * Return : 0 always
+ */
+
+int main(void)
+{
+	char Chartype;
+	int intType;
+	long int longType;
+	long long int longlongType;
+	float floatType;
+	/*sizeof * sizeof prints the size of the various datatypes*/
+	printf("Size of a char: %zu bytes\n", sizeof(Chartype));
+	printf("Size of an int: %zu bytes\n", sizeof(intType));
+	printf("Size of a long int: %zu bytes\n", sizeof(longType));
+	printf("Size of long long int: %zu bytes\n", sizeof(longlongType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+	return (0);
+}
