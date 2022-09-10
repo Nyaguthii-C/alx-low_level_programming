ace703e56cc2df22f289759d4130a87c9cb8ea8d
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-09-10 09:04:42 -0700

    script update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - this is the entry point for the program
- * Description: main- prints \"Programming is like building a multilingual puzzle
+ * Description: main- prints "Programming is like building a multilingual puzzle
  * Return : 0 always
- * Description : a return value at the end of main to end the program
  */
 
 int main(void)
