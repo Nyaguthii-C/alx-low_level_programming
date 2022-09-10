2dc20343a532679332fbc14d1d5762200fb333fe
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-09-10 03:12:03 -0700

    print script using puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * This function prints a given string
+ * The sting is printed using puts()
+*/
+
+int main()
+{
+  puts(" \"Programming is like building a multilingual puzzle");
+}  
+
