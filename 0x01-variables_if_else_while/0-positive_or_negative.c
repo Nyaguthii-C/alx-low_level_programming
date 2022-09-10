1b165defa41cb7f08a75e58986b6c367901435a7
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-09-10 01:39:00 -0700

    script update compile code

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,7 +8,7 @@
 
 /* betty style doc for function main goes there */
 
-int main(void) 
+int main(void)
 
 {
 
@@ -18,10 +18,11 @@ int main(void)
 			srand(time(0));
 
 				n = rand() - RAND_MAX / 2;
-
-					if (n > 0)																															{
+					if (n > 0)
+					{
 						printf(n, "%i is positive\n");
-					}																else if (n == 0)	
+					}								
+					else if (n == 0)	
 					{
 						printf(n, "%i is zero\n");
 					}
