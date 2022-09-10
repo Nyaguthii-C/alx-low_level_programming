ce344015bbe35268b894e4eff7939578e3472d31
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-09-10 08:52:51 -0700

    edit return description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * main - this is the entry point for the program
  * Description:the main function contains other  functions to execute
  *
- *return : 0
+ * Return : 0 always
  * Description : a return value at the end of main to end the program
  */
 
@@ -12,8 +12,6 @@ int main(void)
 {
 	/**
 	 * puts()- prints the string in a newline
-	 *
-	 * return (0) - a return value at the end of main to end the program
 	 */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
