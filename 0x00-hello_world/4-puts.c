e4f81719b0cbcdc584a0c0e5422fd41f356e5557
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-09-10 04:43:20 -0700

    leading /* on a separate line in comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,8 +8,10 @@
 
 int main(void)
 {
-	/* puts()- prints the string in a newline
-	 * return (0) - a return value at the end of main to end the program 
+	/**
+	 * puts()- prints the string in a newline
+	 *
+	 * return (0) - a return value at the end of main to end the program
 	 */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
