8742efb455271d8f7d8ca3bd52b6e3a2ce427abc
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-09-10 14:06:45 -0700

    prints using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+/**
+ * Description: main -prints with proper grammar, but the ...
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	/**
+	 *printf()- prints with proper grammar,but the outcome ...
+	 */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return	(0);
+}
