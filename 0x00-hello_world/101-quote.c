cd64c535c0aa3350428b4b021737cfed748a032a
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-09-11 03:42:45 -0700

    print quote to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * Return:0 always
+ */
+
+int main(void)
+{
+	/*fprintf() prints to standard error stderr*/
+	fprintf("and that piece of art is useful\"
+			"NC "- Dora Korpar, 2015-10-19\n", stderr);
+	return (0);
