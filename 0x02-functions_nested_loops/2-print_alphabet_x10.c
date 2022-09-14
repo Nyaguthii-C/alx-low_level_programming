59375dc25ddfdaec0533cd48f32900150df5347f
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-09-14 16:41:36 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet ten times
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char j, x;
+
+	for (j = '0'; j < 10 ; j++)
+	{
+		for (x = 'a'; x < = 'z'; x++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n')
+	}
+	return (0);
+}
