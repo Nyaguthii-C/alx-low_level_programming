a444cbc958a764b730c5e26bc818bde5ac52a419
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-09-19 16:17:18 -0700

    update the valuethe pointer points at

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the value of the variable usin a pointer
+ * @n: pointer variable
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
