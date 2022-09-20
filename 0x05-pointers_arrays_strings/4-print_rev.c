3134eccce3f45b41e430887a450329786ce304ad
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-09-20 16:33:45 -0700

    prints a string, in reverse, followed by a new line

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - print_rev
+ * _putchar - inserts character
+ * @s: pointer
+ *
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
