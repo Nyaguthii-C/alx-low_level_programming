8e8ce07d9efd10accf6ecf8ac00f2af93fe06c43
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-09-25 15:38:21 +0300

    initial commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _strcmp -compares the strings
+ * 
+ * Return: if s1 > s2 return negative number
+ * 		if s2 > s1 return positive number
+ * 		if s1 = s2 return 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0,
+	diff = 0;
+
+	if (s1[i] == '\0' && s2[i] == '\0')
+	{
+		break;
+	}
+	else if (s1[i] == '\0')
+	{
+		diff = s2[i]
+		break;
+	}
+	else if (s2[i] == '\0')
+	{	diff = s1[i]
+       		break;	
+	}	
+	else if (s1[i] != s2[i]
+	{
+ 		diff = s1[i] - s2[i];
+	  	break;
+	}
+	else
+		i++;
+}
+
