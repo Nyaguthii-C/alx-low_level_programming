6b33f32f3993906e8cb4e5d5e03e6634960865e5
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-09-26 19:26:29 -0700

    time counter

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of Jack Bauer day
+ *
+ *
+ */
+void jack_bauer(void)
+{
+	int i = 1;
+
+	while (i <= 0000 && i < 2359)
+	{
+		i++;
+	}
+}
