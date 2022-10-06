a4a88e515c29d8d980dd0a496a849dcd092164a4
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-10-05 20:51:46 -0700

    functions

---
+++ 0x09-static_libraries/isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _isalpha - checks if character entered is an alphabet
+* @c: character to be checked
+*
+* Return: On success 0
+*/
+int _isalpha(int c)
+{
+	for (c = 'a' && c <= 'z' || c = 'A' && c <= 'Z')
+	{
+		_putchar(c);
+	}
+}
