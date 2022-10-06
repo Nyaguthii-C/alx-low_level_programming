f217ab32f1e10c3f46b9ef03a150e8d7d7915786
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-10-05 21:54:56 -0700

    find length of a fixed size string

---
+++ 0x09-static_libraries/strnlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _strlen - funds length of string
+* @s: string
+*
+* Return: the counter;
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+	int n;
+
+	for (i < n; *s != '\0'; ++s)
+	{
+		i++;
+		return (i);
+	}
+}
