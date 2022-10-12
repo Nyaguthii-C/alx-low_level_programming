74f4d8f26dbd1bd04155dd0dd154956827deb582
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-10-12 15:13:23 -0700

    add strpstr function to static library

---
+++ 0x09-static_libraries/strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+* _strstr - finds first occurence of substring needle in in string haystack
+* @needle: string searched in haystack
+* @haystack: string cthat may contain needle
+* Return: pointer to first occurence of needle
+* else return NULL
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	while (haystack)
+	{
+		for (i = 0; i++;)
+		{
+			if (haystack[i] == needle[i])
+			{
+				return (needle);
+				i++;
+			}
+			else
+			{
+				return (NULL);
+			}
+		}
+		haystack++;
+	}
+	return (0);
+}
