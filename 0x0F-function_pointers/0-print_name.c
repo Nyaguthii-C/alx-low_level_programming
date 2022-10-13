047f357b2ba2f48a8090f80a3eb42dd46d4fc417
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-10-13 16:39:19 -0700

    prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_name - prints the name
+* @name: pointer to name
+* @f: pointer to function
+*
+* Return: nothing
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+	f(name);
+}
