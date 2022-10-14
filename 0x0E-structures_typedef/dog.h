db6b781ccde8d0d1e1d37019edab262697e22971
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-10-14 16:01:36 -0700

    create structure for dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+*struct dog - structure for dog
+* @name: pointer for element in structure dog
+* @age: age element in struct
+* @owner: pointer owner element
+*/
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*DOG_H*/
