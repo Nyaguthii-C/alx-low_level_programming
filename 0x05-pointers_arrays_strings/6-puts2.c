87484ceb82fc16eadd5045711c7d75833190f470
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-10-14 21:14:46 -0700

    print every other character of a string followed by new line

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* puts2 - prints every other character of a string
+* @str: string from which characters are printed
+*
+* Return: nothing
+*/
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		putchar(str[i]);
+		putchar('\n');
+	}
+}
