9fcf62c626c95929cc53f5883763369386870e60
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-10-22 20:00:54 -0700

    utilize the new variable in function

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,11 +13,12 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -in;
+
+		in = -in;
 	}
 	if ((in / 10) > 0)
 	{
-		print_number(n / 10);
+		print_number(in / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar((in % 10) + '0');
 }
