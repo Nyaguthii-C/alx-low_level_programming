144ff0d8a2cfa1fedaa950c068edbf0456621a44
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-10-30 19:24:07 -0700

    change return value for malloc

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -18,7 +18,11 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	{
 		return (NULL);
 	}
+	if (head == NULL)
+	{
+		return (NULL);
+	}
 	ptr->next = *head;
 	*head = ptr;
-	return (*head);
+	return (ptr);
 }
