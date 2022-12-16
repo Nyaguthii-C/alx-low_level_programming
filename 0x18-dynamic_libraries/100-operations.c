29b32e64908251abf7808149bb6bfbc4bfebcced
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-12-16 02:45:07 -0800

    add operation functions to dynamic library

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* cops_add - returns the sum of integers a and b
+* @a: integer a
+* @b: integer b
+* Return: adition of a and b
+*/
+int cops_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+* cops_sub - returns subtraction of integer b from a
+* @a: integer numerator
+* @b: integer denominator
+* Return: value after subtracting  from a
+*/
+int cops_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+* cops_mul - returns subtraction of integer b from a cops_sub
+* @a: integer a
+* @b: integer b
+* Return: value after mul
+*/
+int cops_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+* cops_div - returns the quotient of a dividision of a and b
+* @a: integer a
+* @b: integer b
+* Return: division of a by b
+*/
+int cops_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+* cops_mod - returns the modulo of a divide by b
+* @a: int a
+* @b: int b
+* Return: modulus of a and b
+*/
+int cops_mod(int a, int b)
+{
+	return (a % b);
+}
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -21,5 +21,10 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int cops_add(int a, int b);
+int cops_sub(int a, int b);
+int cops_mul(int a, int b);
+int cops_div(int a, int b);
+int cops_mod(int a, int b);
 
 #endif /*MAIN_H*/
