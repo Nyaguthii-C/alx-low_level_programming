3e41a02d61f63714525cefe626375258a6c64e52
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-12-17 11:18:56 -0800

    add new fn prototypes

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -21,10 +21,10 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int cops_add(int a, int b);
-int cops_sub(int a, int b);
-int cops_mul(int a, int b);
-int cops_div(int a, int b);
-int cops_mod(int a, int b);
+int cop_add(int a, int b);
+int cop_sub(int a, int b);
+int cop_mul(int a, int b);
+int cop_div(int a, int b);
+int cop_mod(int a, int b);
 
 #endif /*MAIN_H*/
