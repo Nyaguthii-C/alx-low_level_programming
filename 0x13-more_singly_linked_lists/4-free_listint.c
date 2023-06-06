50d12ed5284fc9b3532b3e549c5511738d10441b
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-06-06 11:58:00 -0700

    free a list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,10 +8,10 @@ void free_listint(listint_t *head)
 {
 	listint_t *tmp;
 
-	tmp = head;
-	if (head != NULL)
+	while (head != NULL)
 	{
-		head = head->next;
-		free(tmp);
+		tmp = head->next;
+		free(head);
+		head = tmp;
 	}
 }
